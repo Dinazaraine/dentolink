f0319de31da401ac2d5bcc00e77c4eeb5224f7fd
Dinazaraine <167070500+Dinazaraine@users.noreply.github.com>
2025-09-02 16:36:01 +0300

    Update database.js
    pour le deployement

--- backEnd/dist/config/database.js
+++ backEnd/dist/config/database.js
@@ -7,14 +7,13 @@ const mysql = require("mysql2"); // API callback (pas de /promise)
 
 /** Pool de connexions MySQL */
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "127.0.0.1",
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  process.env.DATABASE_URL || {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
+  }
 });
 
 /** Helper de requête (callbacks) */
